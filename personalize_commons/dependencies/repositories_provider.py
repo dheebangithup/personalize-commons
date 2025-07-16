5acd4f43353cf94b33200bcbffec0dd8a2a20cf6
dheebangithup <ddheepu@yahoo.com>
2025-07-16 16:39:40 +0530

    Repo provider and service added

---
+++ personalize_commons/dependencies/repositories_provider.py
@@ -0,0 +1,48 @@
+# src/dependencies/repositories.py
+
+from personalize_commons.repositories.campaign_repository import CampaignRepository
+from personalize_commons.repositories.item_repository import ItemRepository
+from personalize_commons.repositories.recommendation_repository import RecommendationRepository
+from personalize_commons.repositories.tenant_repository import TenantRepository
+from personalize_commons.repositories.user_repository import UserRepository
+
+from personalize_commons.dependencies.aws_providers import get_dynamodb_resource, get_dynamodb_client
+
+# Create singleton instances
+__user_repository = None
+__campaign_repository = None
+__item_repository = None
+__recommendation_repository = None
+__tenant_repository = None
+
+
+def get_tenant_repository():
+    global __tenant_repository
+    if __tenant_repository is None:
+        __tenant_repository = TenantRepository(resource=get_dynamodb_resource())
+    return __tenant_repository
+
+def get_user_repository():
+    global __user_repository
+    if __user_repository is None:
+        __user_repository = UserRepository(client=get_dynamodb_client())
+    return __user_repository
+
+
+def get_campaign_repository():
+    global __campaign_repository
+    if __campaign_repository is None:
+        __campaign_repository = CampaignRepository(resource=get_dynamodb_resource())
+    return __campaign_repository
+
+def get_item_repository():
+    global __item_repository
+    if __item_repository is None:
+        __item_repository = ItemRepository(resource=get_dynamodb_resource())
+    return __item_repository
+
+def get_recommendation_repository():
+    global __recommendation_repository
+    if __recommendation_repository is None:
+        __recommendation_repository = RecommendationRepository(resource=get_dynamodb_resource())
+    return __recommendation_repository
--- personalize_commons/dependencies/service_provider.py
+++ personalize_commons/dependencies/service_provider.py
@@ -1,5 +1,7 @@
 from personalize_commons.dependencies.aws_providers import get_s3_client
+from personalize_commons.dependencies.repositories_provider import get_user_repository
 from personalize_commons.services.s3_service import S3Service
+from personalize_commons.services.user_service import UserService
 
 __s3_service = None
 
@@ -12,3 +14,12 @@ def get_s3_service() -> S3Service:
         __s3_service = S3Service(client=get_s3_client())
 
     return __s3_service
+
+def get_user_service() -> UserService:
+    """Dependency provider for UserService (singleton)"""
+    global _user_service
+
+    if _user_service is None:
+        _user_service = UserService(user_repo=get_user_repository())
+
+    return _user_service
