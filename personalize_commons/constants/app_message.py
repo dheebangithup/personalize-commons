b1a8bb1e8115d0acdde84ef6d95228b0ada64159
dheebangithup <ddheepu@yahoo.com>
2025-07-16 15:34:33 +0530

    constnas provider added

---
+++ personalize_commons/constants/app_message.py
@@ -0,0 +1,16 @@
+class AppMessage:
+    RECOM_NOT_FOUND = 'recommendation not found'
+    CAMPAIGN_NOT_FOUND = 'campaign_not_found'
+    SUCCESS = "success"
+    ERROR = "error"
+    WARNING = "warning"
+    INFO = "info"
+    DEBUG = "debug"
+    USERS_NOT_MATCHING = "users not matching with target segment"
+    USERS_NOT_MATCHING_IN_AI = "users not matching with targeting item"
+    TENANT_NOT_FOUND = "tenant not found"
+    ITEM_ID_REQUIRED = "item_id must be provided if recommendation-logic is not 'items_to_user'"
+    RECOM_ID="recommendation_id"
+    RECOM_STARTED="recommendation started successfully"
+    RECOM_FAILED="recommendation failed"
+
