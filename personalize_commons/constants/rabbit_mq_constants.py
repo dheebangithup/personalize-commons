08c0c62347a4bfab336b40f57b4abbf3c5982eca
dheebangithup <ddheepu@yahoo.com>
2025-07-16 09:39:55 +0530

    rabbitmq constants added

--- personalize_commons/constants/rabbit_mq_constants.py
+++ personalize_commons/constants/rabbit_mq_constants.py
@@ -1,5 +1,6 @@
 class RabbitMQConstants:
     EXCHANGE_NAME = 'personalize'
+    DLQ_EXCHANGE_NAME = 'personalize.dlq'
     RECOMMENDATION_QUEUE = 'recommendation'
     RECOMMENDATION_DLQ = "recommendations.dlq"
     RECOMMENDATION_START_ROUTING_KEY = 'personalize.start'
