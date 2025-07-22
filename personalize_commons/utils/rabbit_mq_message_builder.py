ed7177958bf56ee28e03dd1c266eea168ed20b3a
dheebangithup <ddheepu@yahoo.com>
2025-07-22 19:40:51 +0530

    Time util updated

--- personalize_commons/utils/rabbit_mq_message_builder.py
+++ personalize_commons/utils/rabbit_mq_message_builder.py
@@ -1,10 +1,9 @@
 import json
-from datetime import datetime, timezone
 from typing import Dict, Any
 
 from personalize_commons.constants.event_type import EventType
 from personalize_commons.constants.rabbit_mq_constants import RabbitMQConstants
-from personalize_commons.utils.datetime_utils import utc_now_iso
+from personalize_commons.utils.datetime_utils import ist_now_iso
 
 
 class RabbitMqMessageBuilder:
@@ -18,7 +17,7 @@ class RabbitMqMessageBuilder:
         message = {
             RabbitMQConstants.Payload.EVENT_TYPE: str(event_type),
             RabbitMQConstants.Payload.SOURCE:source,
-            RabbitMQConstants.Payload.TIMESTAMP: utc_now_iso(),
+            RabbitMQConstants.Payload.TIMESTAMP: ist_now_iso(),
             RabbitMQConstants.Payload.PAYLOAD: payload,
             RabbitMQConstants.Payload.RETRY_COUNT: retry_count
         }
