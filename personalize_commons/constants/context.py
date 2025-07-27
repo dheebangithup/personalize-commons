b1584be8313d0061d0460ca3d73fd546ebcf74ce
dheebangithup <ddheepu@yahoo.com>
2025-07-27 18:32:23 +0530

    REVERT

---
+++ personalize_commons/constants/context.py
@@ -0,0 +1,6 @@
+# context.py
+import contextvars
+
+from personalize_commons.constants.app_constants import AppConstants
+
+tenant_id_ctx = contextvars.ContextVar(AppConstants.X_MDM_PERSONALIZE, default=None)
